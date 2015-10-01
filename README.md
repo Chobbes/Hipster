5b27a1c48d71b61e3767e85a758c267a10523a76
Calvin Beck <hobbes@ualberta.ca>
2015-10-01 11:18:34 -0600

    Free monad goo.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Hipster
 A Haskell MIPS EDSL. Mostly for "educational purposes" (TM). More seriously the goal of this is to explore assembly DSLs in Haskell, and see how they're put together and what makes them tick. Can these be useful? Probably! One of the main goals is to allow for composable abstractions of assembly code, and automatic register allocation / spilling. In theory this could be more general and support multiple backends (after all there is overlap with LLVM's functionality), but an additional goal is to be able to generate readable, and documented assembly code. Because of this, it's beneficial to stick to one target assembly language, at least for experimentation!
+
+# Labels
+We want to be able to create named labels for use in the assembly code in order to aid with readability. Naively this would cause problems with creating abstractions as any named labels within an abstraction (say a loop), would be problematic since if the abstraction is used multiple times the label will be repeated. To get around this issue we allow labels to be allocated with a name, but a unique number may be appended to the end when the labels are actually compiled.
+
+Keeping track of procedure names, and allowing the assembly lanuage to use this for prefixes to labels may also be ideal.
+
+# Comments
+We want the user to be able to write well commented assembly code. This means we are going to have to have comments at the label level, and at the assembly level. Additionally some formatting primitives must be available.
--- src/Language/Hipster/AST.hs
+++ src/Language/Hipster/AST.hs
@@ -1,45 +1,66 @@
-module AST where
+module Language.Hipster.AST where
 
--- | All of the MIPS 32 hardware registers.
-data MipsRegister
-    = Zero
-    | AT
-    | V0
-    | V1
-    | A0
-    | A1
-    | A2
-    | A3
-    | T0
-    | T1
-    | T2
-    | T3
-    | T4
-    | T5
-    | T6
-    | T7
-    | S0
-    | S1
-    | S2
-    | S3
-    | S4
-    | S5
-    | S6
-    | S7
-    | T8
-    | T9
-    | K0
-    | K1
-    | GP
-    | SP
-    | FP
-    | RA
+-- import Control.Monad.Free hiding (Free, Pure)
+import Control.Monad.Trans.Free  -- hiding (Free, Pure)
+import Control.Monad.Trans.Class
+import Compiler.Hoopl
+import Data.Map as M
 
-type Id = Integer
 
 -- | Data type for register allocation.
 data Register
-     = Unique Id  -- ^ Any general purpose MIPS 32 register can be allocated for this.
-     | Specific MipsRegister -- ^ Specific register reserved.
+     = Var Unique  -- ^ Any general purpose MIPS 32 register can be allocated for this.
+     | Reg Integer -- ^ Specific register reserved.
+     deriving (Show)
 
-data AST = Add Register Register
+-- | Immediate values in MIPS.
+type Immediate = Integer
+
+-- | Data type representing MIPS instructions.
+data Inst
+    = ADD Register Register Register
+    | ADDU Register Register Register
+    | ADDI Register Register Immediate
+    | ADDIU Register Register Immediate
+    | SUB Register Register Register
+    | SUBU Register Register Register
+    | SUBI Register Register Immediate
+    | SUBIU Register Register Immediate
+    | MULT Register Register
+    | MULTU Register Register
+    | DIV Register Register
+    | DIVU Register Register
+    deriving (Show)
+
+-- | MIPS assembly is essentially a list of instructions, which is what
+-- the Free monad gives us.
+type MipsASM a = FreeT ((,) Inst) SimpleUniqueMonad a
+
+
+-- | Convert a MIPS program to a list of instructions.
+compile :: MipsASM a -> [Inst]
+compile = runSimpleUniqueMonad . compile'
+  where compile' free =
+               do x <- runFreeT free
+                  case x of
+                    (Pure a) -> return []
+                    (Free (inst, fs)) -> fmap (inst :) $ compile' fs
+
+
+-- | Allocate a new 32-bit value (any register will do).
+newVar :: MipsASM Register
+newVar = do unique <- lift $ freshUnique
+            return $ Var unique
+
+
+add :: Register -> Register -> Register -> MipsASM Register
+add d s t = liftF (ADD d s t, d)
+
+sub :: Register -> Register -> Register -> MipsASM Register
+sub d s t = liftF (SUB d s t, d)
+
+
+-- | An actual MIPS program contains labels.
+type LabelMap = M.Map String Integer
+
+type MipsProgram a = FreeT ((,) )
